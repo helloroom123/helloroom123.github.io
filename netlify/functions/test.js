1180e87ab62e4b556386ba24d0a33a659332a9e4
helloroom123 <2815918449@qq.com>
2026-01-18 17:35:23 +0800

    Add test function to debug Netlify

---
+++ netlify/functions/test.js
@@ -0,0 +1,17 @@
+exports.handler = async (event, context) => {
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS'
+  };
+
+  if (event.httpMethod === 'OPTIONS') {
+    return { statusCode: 200, headers, body: '' };
+  }
+
+  return {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({ message: "Test function works!", method: event.httpMethod })
+  };
+};
